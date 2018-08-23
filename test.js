a1ec04b5cf62403c6b7d8cefb368a2d7596065bf
JungTaeHyuk <hyukkiller@gmail.com>
2018-08-23 14:06:58 +0900

    vue commit

---
+++ test.js
@@ -0,0 +1,86 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: '안녕하세요 Vue!'
+  }
+});
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
+    ,message2: '이 화면은 ' + new Date() + ' 에 로드 되었습니다'
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'JavaScript 배우기' },
+      { text: 'Vue 배우기' },
+      { text: '무언가 멋진 것을 만들기' }
+    ]
+,
+     todos2: [
+      { text: 'JavaScript 배우기' },
+      { text: 'Vue 배우기' },
+      { text: '무언가 멋진 것을 만들기' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: '안녕하세요! Vue.js!'
+    ,message2: '뒤집어집니다!'
+    ,seen: false
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+      alert( this.message2  );
+      
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: '안녕하세요 Vue!'
+  }
+})
+
+// todo-item 이름을 가진 컴포넌트를 정의합니다
+Vue.component('jth-item', {
+  template: '<h1>할일 항목 하나입니다.</h1>'
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+new Vue({
+  el: '#app-6-5'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
